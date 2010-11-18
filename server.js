81408187163cffcd9af45bf829acef0f02969edc
Cosmin Gorgovan <cosmin@linux-geek.org>
2010-11-18 15:32:35 +0000

    AGPL license

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+/*  Polyglossy is a language detection web API.
+    Copyright (C) 2010 Cosmin Gorgovan <cosmin AT linux-geek.org>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // Settings go here
 var port_no = 8431
 var language_models = './data_2nd_order.json'
